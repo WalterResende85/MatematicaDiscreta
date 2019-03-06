204af1047b34053f6650eb57a4fe4fac75d9d1d0
WalterResende85 <walterccr@hotmail.com>
2019-03-06 09:32:05 -0300

    tentando contidoNaocontido

--- src/matematicadiscreta/MatematicaDiscreta.java
+++ src/matematicadiscreta/MatematicaDiscreta.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MatematicaDiscreta {
@@ -51,38 +52,50 @@ public class MatematicaDiscreta {
             System.out.println("");
             System.out.println("Escolha uma opção do menu");
             System.out.println("1  - Pertence ou não Pertence.");
-            System.out.println("3  - Contido ou igual / Não contido ou igual.");
-            System.out.println("5  - Contido propriamente / Não contido propriamente.");
-            System.out.println("7  - União.");
-            System.out.println("8  - Interseção.");
-            System.out.println("9  - Produto cartesiano.");
-            System.out.println("10 - Conjunto das partes.");
+            System.out.println("2  - Contido ou igual / Não contido ou igual.");
+            System.out.println("3  - Contido propriamente / Não contido propriamente.");
+            System.out.println("4  - União.");
+            System.out.println("5  - Interseção.");
+            System.out.println("6  - Produto cartesiano.");
+            System.out.println("7 - Conjunto das partes.");
             System.out.println("0  - Sair");
             menu = ler.nextInt();
 
             if (menu == 1) {
-               pertenceNaoPeretence(conjuntos.get(0) , elementos.get(0));
+                if (pertenceNaoPertence(conjuntos.get(0), elementos.get(0).conteudo)) {
+                    System.out.println("********************************");
+                    System.out.println("Pertence");
+                    System.out.println("********************************");
+                } else {
+                    System.out.println("********************************");
+                    System.out.println("Não Pertence");
+                    System.out.println("********************************");
+                }
+            }
+
+            if (menu == 2) {
+                if(contidoNaocontido(conjuntos.get(0), conjuntos.get(1))){
+                    System.out.println("Está contido");
+                }else{
+                    System.out.println("Não está contido");
+                }
                 
             }
-           
+
             if (menu == 3) {
 
             }
-           
+
+            if (menu == 4) {
+
+            }
             if (menu == 5) {
 
             }
-           
+            if (menu == 6) {
+
+            }
             if (menu == 7) {
-
-            }
-            if (menu == 8) {
-
-            }
-            if (menu == 9) {
-
-            }
-            if (menu == 10) {
 
             }
 
@@ -97,25 +110,46 @@ public class MatematicaDiscreta {
             return "elemento";
         }
     }
-     private static boolean pertenceNaoPeretence(Conjunto c, Elemento e ){
-                boolean pertence = false;
-                for(int i=0; i<c.getElementos().size(); i++){
-                    if(e.conteudo == i){
-                        pertence =true;
-                        break;
-                    }
+
+    private static boolean pertenceNaoPertence(Conjunto c, Integer e) {
+        boolean pertence = false;
+        for (int i = 0; i <= c.getElementos().size(); i++) {
+            if (e == i) {
+                pertence = true;
+                break;
+            }
+        }
+
+        return pertence;
+    }
+
+    private static boolean contidoNaocontido(Conjunto c1, Conjunto c2) {
+        boolean contido = true;
+        for (Integer e : c1.elementos) {
+            if (!pertenceNaoPertence(c2, e)) {
+                contido = false;
+                break;
+            }
+        }
+        return contido;
+        /*for (int i = 0; i < c1.elementos.size(); i++) {
+            contido = false;
+            for (int j = 0; j < c2.elementos.size(); j++) {
+                if (c1.elementos.get(i) == c2.elementos.get(j)) {
+                    contido = true;
                 }
-                if(pertence){
-                    System.out.println("****************");
-                    System.out.println("Pertence");
-                     System.out.println("****************");
-                }else{
-                     System.out.println("****************");
-                    System.out.println("Não Pertence");
-                     System.out.println("****************");
-                }
-                
-                return pertence;
-                }
-     
+            }
+            if (!contido) {
+                break;
+            }
+        }
+        if (contido) {
+            System.out.println("Está contido");
+        } else {
+            System.out.println("não está contido");
+        }
+         */
+
+    }
+
 }
