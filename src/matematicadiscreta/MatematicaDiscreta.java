9692dbce12289dc93a84590afcc40d4d36d30624
WalterResende85 <walterccr@hotmail.com>
2019-03-11 17:07:48 -0300

    contidoNaoContido

--- src/matematicadiscreta/MatematicaDiscreta.java
+++ src/matematicadiscreta/MatematicaDiscreta.java
@@ -9,16 +9,17 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.TreeSet;
 import org.apache.commons.lang3.SerializationUtils;
-public class MatematicaDiscreta implements Serializable{
-    
+
+public class MatematicaDiscreta implements Serializable {
+
     public static void main(String[] args) {
         ArrayList<Conjunto> conjuntos = new ArrayList();
         ArrayList<Elemento> elementos = new ArrayList();
-        
+
         int c = 1;
         int menu = 99;
         Scanner ler = new Scanner(System.in);
-        
+
         do {
             System.out.println("Informe o nome de arquivo texto:\n");
             try {
@@ -26,7 +27,7 @@ public class MatematicaDiscreta implements Serializable{
                 String nome = ler.nextLine();
                 FileReader arq = new FileReader(nome + ".txt");
                 BufferedReader lerArq = new BufferedReader(arq);
-                
+
                 String linha = lerArq.readLine(); // lê a primeira linha
 
                 while (linha != null) {
@@ -38,17 +39,17 @@ public class MatematicaDiscreta implements Serializable{
                     linha = lerArq.readLine(); // lê da segunda até a última linha
 
                 }
-                
+
                 arq.close();
             } catch (IOException e) {
-                
+
                 c = 1;
                 System.err.printf("Erro na abertura do arquivo: %s.\n",
                         e.getMessage());
             }
-            
+
         } while (c == 1);
-        
+
         do {
             System.out.println("");
             System.out.println("____________________________________________________________");
@@ -63,9 +64,9 @@ public class MatematicaDiscreta implements Serializable{
             System.out.println("0  - Sair");
             System.out.println("____________________________________________________________");
             menu = ler.nextInt();
-            
+
             if (menu == 1) {
-                if (pertenceNaoPertence(conjuntos.get(0), elementos.get(0).conteudo)) {
+                if (pertenceNaoPertence(conjuntos.get(1), elementos.get(1).conteudo)) {
                     System.out.println("********************************");
                     System.out.println("Pertence");
                     System.out.println("********************************");
@@ -75,45 +76,60 @@ public class MatematicaDiscreta implements Serializable{
                     System.out.println("********************************");
                 }
             }
-            
+
             if (menu == 2) {
                 if (contidoNaoContido(conjuntos.get(0), conjuntos.get(1))) {
+                    System.out.println("********************************");
                     System.out.println(conjuntos.get(0) + " Está contido em " + conjuntos.get(1));
+                    System.out.println("********************************");
                 } else {
+                    System.out.println("********************************");
                     System.out.println(conjuntos.get(0) + " Não está contido em " + conjuntos.get(1));
+                    System.out.println("********************************");
                 }
-                
+
             }
-            
+
             if (menu == 3) {
-                if (contidoNaoContido(conjuntos.get(1), conjuntos.get(0))) {
-                    System.out.println(conjuntos.get(0) + " Está apenas contido em " + conjuntos.get(1));
-                } else {
-                    System.out.println(conjuntos.get(0) + " Está contido propriamente em " + conjuntos.get(1));
+                if (contidoNaoContido(conjuntos.get(0), conjuntos.get(1))== contidoNaoContido(conjuntos.get(1), conjuntos.get(0))) {
+                    System.out.println("********************************");
+                    System.out.println(conjuntos.get(0) + " Está apenas contido em " + conjuntos.get(1)+", mas não contido propriamente");
+                                        
+                    System.out.println("********************************");
+                }else if (contidoNaoContido(conjuntos.get(0), conjuntos.get(1))&& conjuntos.get(1).elementos.size() > conjuntos.get(0).elementos.size() ) {
+                    System.out.println("********************************");
+                    System.out.println(conjuntos.get(0) + " Está  contido propriamente " + conjuntos.get(1));
+                    System.out.println("********************************");
                 }
             }
-            
+
             if (menu == 4) {
+                System.out.println("********************************");
                 System.out.print(
                         uniaoDeConjuntos(conjuntos).getIdentificador() + "=");
                 uniaoDeConjuntos(conjuntos).imprimeConjunto();
-                
+                System.out.println("");
+                System.out.println("********************************");
+
             }
             if (menu == 5) {
-                System.out.print(intersecaoDeConjuntos(conjuntos).getIdentificador()+"=");
+                System.out.println("********************************");
+                System.out.print(intersecaoDeConjuntos(conjuntos).getIdentificador() + "=");
                 intersecaoDeConjuntos(conjuntos).imprimeConjunto();
+                System.out.println("");
+                System.out.println("********************************");
             }
             if (menu == 6) {
                 produtoCartesiano(conjuntos.get(0), conjuntos.get(1));
             }
             if (menu == 7) {
                 conjuntoDasPartes(conjuntos);
-                
+
             }
-            
+
         } while (menu != 0);
     }
-    
+
     public static String verificador(String a) {
         a.trim();
         if (!Character.isLowerCase(a.charAt(0))) {
@@ -122,20 +138,20 @@ public class MatematicaDiscreta implements Serializable{
             return "elemento";
         }
     }
-    
+
     private static boolean pertenceNaoPertence(Conjunto c, Integer e) {
         boolean pertence = false;
-        
+
         for (int i = 0; i < c.getElementos().size(); i++) {
             if (e == c.getElementos().get(i)) {
                 pertence = true;
                 break;
             }
         }
-        
+
         return pertence;
     }
-    
+
     private static boolean contidoNaoContido(Conjunto c1, Conjunto c2) {
         boolean contido = true;
         for (Integer e : c1.elementos) {
@@ -145,26 +161,8 @@ public class MatematicaDiscreta implements Serializable{
             }
         }
         return contido;
-        /*for (int i = 0; i < c1.elementos.size(); i++) {
-            contido = false;
-            for (int j = 0; j < c2.elementos.size(); j++) {
-                if (c1.elementos.get(i) == c2.elementos.get(j)) {
-                    contido = true;
-                }
-            }
-            if (!contido) {
-                break;
-            }
-        }
-        if (contido) {
-            System.out.println("Está contido");
-        } else {
-            System.out.println("não está contido");
-        }
-         */
-        
     }
-    
+
     private static Conjunto uniaoDeConjuntos(ArrayList<Conjunto> c1) {
         Conjunto uniao = new Conjunto();
         for (Conjunto c : c1) {
@@ -177,10 +175,10 @@ public class MatematicaDiscreta implements Serializable{
         uniao.setIdentificador("Conjunto Uniao");
         return uniao;
     }
-    
+
     private static Conjunto intersecaoDeConjuntos(ArrayList<Conjunto> c1) {
         boolean controlador = true;
-        
+
         Conjunto acumulador = new Conjunto();
         acumulador.setIdentificador("Conjunto Intersecao");
         for (Conjunto conjunto : c1) {
@@ -193,31 +191,34 @@ public class MatematicaDiscreta implements Serializable{
                     }
                 }
                 if (controlador) {
-                    
+
                     if (!acumulador.elementos.contains(e)) {
                         acumulador.elementos.add(e);
-                        
+
                     }
-                    
+
                 }
             }
         }
         return acumulador;
     }
-    
+
     public static void produtoCartesiano(Conjunto c1, Conjunto c2) {
         Scanner ler = new Scanner(System.in);
         ArrayList<String> cartesiano = new ArrayList();
         for (Integer a : c1.elementos) {
             for (Integer b : c2.elementos) {
                 cartesiano.add("<" + a + "," + b + ">");
-                
+
             }
         }
+        System.out.println("********************************");
         System.out.println("Produto Cartesiano de " + c1.getIdentificador() + " com " + c2.getIdentificador());
         System.out.println(cartesiano);
+        System.out.println("********************************");
         System.out.println("Gostaria de reverter o Produto de " + c1.getIdentificador() + " com " + c2.getIdentificador());
         System.out.println("S ou N");
+        System.out.println("********************************");
         String reverter = ler.next();
         if (reverter.equalsIgnoreCase("s")) {
             Conjunto a = new Conjunto();
@@ -234,52 +235,56 @@ public class MatematicaDiscreta implements Serializable{
                 if (!b.elementos.contains(y)) {
                     b.elementos.add(y);
                 }
-                
+
             }
+            System.out.println("********************************");
             System.out.println("Conjunto A " + a.elementos);
             System.out.println("Conjunto B " + b.elementos);
+            System.out.println("********************************");
         }
     }
-    
-    public static ArrayList<TreeSet<Integer>> conjuntoDasPartes(ArrayList<Conjunto> c1){
+
+    public static ArrayList<TreeSet<Integer>> conjuntoDasPartes(ArrayList<Conjunto> c1) {
         Scanner ler = new Scanner(System.in);
-        Conjunto amostragem = c1.get(0);
+        Conjunto amostragem = c1.get(1);
         ArrayList<TreeSet<Integer>> conjunto = new ArrayList();
-         conjunto.add(new TreeSet());
+        conjunto.add(new TreeSet());
         for (Integer e : amostragem.elementos) {
             conjunto.add(new TreeSet());
-            conjunto.get(conjunto.size()-1).add(e);
-            
+            conjunto.get(conjunto.size() - 1).add(e);
+
         }
         for (int i = 0; i < amostragem.elementos.size(); i++) {
-           ArrayList<TreeSet<Integer>> clonado = SerializationUtils.clone(conjunto);
+            ArrayList<TreeSet<Integer>> clonado = SerializationUtils.clone(conjunto);
             for (TreeSet<Integer> combinacao : clonado) {
                 TreeSet combinacaoClonado = SerializationUtils.clone(combinacao);
                 combinacaoClonado.add(amostragem.elementos.get(i));
-                if(!conjunto.contains(combinacaoClonado)){
-                   conjunto.add(combinacaoClonado);
+                if (!conjunto.contains(combinacaoClonado)) {
+                    conjunto.add(combinacaoClonado);
                 }
             }
-           
-           
+
         }
-   
+        System.out.println("********************************");
         System.out.println(conjunto);
-       System.out.println("Gostaria de reverter o conjunto das partes do conjunto "+c1.get(0)+" ?");
+        System.out.println("********************************");
+        System.out.println("Gostaria de reverter o conjunto das partes do conjunto " + c1.get(0) + " ?");
         System.out.println("S ou N");
+        System.out.println("********************************");
         String reverter = ler.next();
         if (reverter.equalsIgnoreCase("s")) {
             Conjunto conjuntoRevertido = new Conjunto();
-            TreeSet<Integer> ultimoTreeSet = conjunto.get(conjunto.size()-1);
-            int x =ultimoTreeSet.size();
-             for (int i = 0; i < x; i++) {
+            TreeSet<Integer> ultimoTreeSet = conjunto.get(conjunto.size() - 1);
+            int x = ultimoTreeSet.size();
+            for (int i = 0; i < x; i++) {
                 conjuntoRevertido.elementos.add(ultimoTreeSet.first());
                 ultimoTreeSet.remove(ultimoTreeSet.first());
             }
-            
-             System.out.println(conjuntoRevertido.elementos);
+            System.out.println("********************************");
+            System.out.println(conjuntoRevertido.elementos);
+            System.out.println("********************************");
         }
-       
+
         return conjunto;
     }
 }
